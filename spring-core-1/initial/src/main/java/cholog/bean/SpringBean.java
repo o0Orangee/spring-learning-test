7f742a329b4330dfb4b06396283664e4d633cb48
o0Orangee <maboongee@gmail.com>
2024-03-30 17:23:13 +0900

    core-1, bean 테스트 통과

--- spring-core-1/initial/src/main/java/cholog/bean/SpringBean.java
+++ spring-core-1/initial/src/main/java/cholog/bean/SpringBean.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 /*
 어떤 어노테이션을 붙였을 때 Bean으로 생성되는지 학습하기
  */
+@Component      //클래스에 @Component 어노테이션 추가 → 스프링 빈으로 등록됨
 public class SpringBean {
     public String hello() {
         return "Hello";
